f08108bd12fc19c312be5b26dfa9d84c239d0271
Aaron <alee870@binghamton.edu>
2018-04-11 19:57:15 -0400

    Initial Progress
    This is where I first felt I had enough worth sharing

---
+++ CS 110 Game Remake/Assets/Assets/Scripts/FrontPage.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FrontPage : MonoBehaviour {
+    Button[] buttons;
+    bool isSoundPlaying = true;
+    public GameObject gameController;
+    AudioSource music;
+	// Use this for initialization
+	void Start () {
+        buttons = GetComponentsInChildren<Button>();
+        for(int i = 0; i < buttons.Length; i++)
+        {
+            Text buttonTexts = buttons[i].GetComponentInChildren<Text>();
+            buttonTexts.text = "";
+        }
+        music = gameController.GetComponent<AudioSource>();
+	}
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("App Quit");
+        Application.Quit();
+    }
+
+    public void ToggleSound()
+    {
+        if (isSoundPlaying)
+        {
+            music.Pause();
+            isSoundPlaying = false;
+        }
+        else
+        {
+            music.UnPause();
+            isSoundPlaying = true;
+        }
+    }
+}
---
+++ CS 110 Game Remake/Assets/Assets/Scripts/Instruction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Instruction : MonoBehaviour {
+    Button MainMenuButton;
+    Text MainMenuButtonText;
+	// Use this for initialization
+	void Start () {
+        MainMenuButton = GetComponentInChildren<Button>();
+        MainMenuButtonText = MainMenuButton.GetComponentInChildren<Text>();
+        MainMenuButtonText.text = "";
+	}
+}
